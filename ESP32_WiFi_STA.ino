4f9285a0822a5e8a2df813b6cbf196ea212e3750
johnnymeow206 <100845072+johnnymeow206@users.noreply.github.com>
2022-05-05 20:15:52 +0800

    Update ESP32_WiFi_STA.ino

--- ESP32_WiFi_STA.ino
+++ ESP32_WiFi_STA.ino
@@ -1,7 +1,7 @@
 #include<WiFi.h>
 
-const char ssid[]="Johnnymeow"; //修改為你家的WiFi網路名稱
-const char pwd[]="10630196"; //修改為你家的WiFi密碼
+const char ssid[]="robin"; //修改為你家的WiFi網路名稱
+const char pwd[]="mochi0607"; //修改為你家的WiFi密碼
 
 void setup() {
   Serial.begin(115200);
